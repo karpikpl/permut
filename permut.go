d752ea4aeaa37bf2388e9c220fc79e43b66b1924
Piotr Karpala <pkarpala@infusion.com>
2015-07-29 10:02:33 +0200

    alogorithm ready but issues wit pointers

--- permut.go
+++ permut.go
@@ -1,65 +1,125 @@
 package main
+
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "strconv"
-  "strings"
-  "os"
-  "math"
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // ReadInts reads whitespace-separated ints from r. If there's an error, it
 // returns the ints successfully read so far as well as the error value.
 func ReadInts(r io.Reader) ([]int, error) {
-    scanner := bufio.NewScanner(r)
-    scanner.Split(bufio.ScanWords)
-    var result []int
-    for scanner.Scan() {
-        x, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-            return result, err
-        }
-        result = append(result, x)
-    }
-    return result, scanner.Err()
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
 }
 
 func Factorial(n int) int {
-  result :=1
-  for i:=1 ; i<=n; i++ {
-    result *= i
-  }
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
 
-  return result
+	return result
+}
+
+func FindPermut2(n int, k int) []int {
+	// find k'th element of n-size permutation
+
+	// there are n! permutations
+	// each number is on first position (n-1)! times
+	totalP := Factorial(n)
+	// 19 -> 20
+	// 4! -> each can be 6 times
+	// 20/6 -> 3.x -> 4
+	// 6 / 3 ->  2
+	result := make([]int, n)
+	result[0] = int(math.Ceil(float64(k+1.0) / (float64(totalP) / float64(n))))
+	result[1] = totalP
+	return result
 }
 
 func FindPermut(n int, k int) []int {
-  // find k'th element of n-size permutation
+	table := make([]int, n)
+	for i := 1; i <= n; i++ {
+		table[i-1] = i
+	}
 
-  // there are n! permutations
-  // each number is on first position (n-1)! times
-  totalP := Factorial(n)
-// 19 -> 20
-// 4! -> each can be 6 times
-// 20/6 -> 3.x -> 4
-// 6 / 3 ->  2
- result := make([]int, n)
- result[0] = int(math.Ceil(float64(k+1.0)/ ( float64(totalP) / float64(n))))
- result[1] = totalP
-  return result
+	tableSlice := table[:]
+	fmt.Printf("table is %T and tableSlice is %T and equal %d \n", table, tableSlice, tableSlice)
+
+	for i := 0; i < k; i++ {
+		tableSlice = PermutMe(tableSlice)
+		fmt.Println("permutation", i+1, "=", tableSlice)
+	}
+
+	return table
+}
+
+func PermutMe(table []int) []int {
+	var k, l int
+	// Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+	// Find the largest index l greater than k such that a[k] < a[l].
+	// Swap the value of a[k] with that of a[l].
+	// Reverse the sequence from a[k + 1] up to and including the final element a[n].
+	for index := len(table) - 1; index > 0; index-- {
+		if table[index] > table[index-1] {
+			k = index - 1
+			break
+		} else {
+			fmt.Println("Last permutation found")
+			return table
+		}
+	}
+	//fmt.Println("Found k!",k)
+
+	for index := len(table) - 1; index > k; index-- {
+		if table[index] > table[k] {
+			l = index
+			break
+		}
+	}
+	//fmt.Println("Found l!",l)
+
+	tmp := table[k]
+	table[k] = table[l]
+	table[l] = tmp
+
+	table = Reverse(table, k+1)
+
+	return table
+}
+
+func Reverse(s []int, from int) []int {
+	for i, j := from, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+
+	return s
 }
 
 func main() {
-    argsWithoutProg := os.Args[1:]
-    var input []int
-    var err error
+	argsWithoutProg := os.Args[1:]
+	var input []int
+	var err error
 
-    if(len(argsWithoutProg) == 0){
-      input, err = ReadInts(os.Stdin)
-    } else {
-      input, err = ReadInts(strings.NewReader(argsWithoutProg[0]));
-    }
+	if len(argsWithoutProg) == 0 {
+		input, err = ReadInts(os.Stdin)
+	} else {
+		input, err = ReadInts(strings.NewReader(argsWithoutProg[0]))
+	}
 
-    fmt.Println(input, err)
+	fmt.Println(input, err)
 }
--- permut_test.go
+++ permut_test.go
@@ -1,68 +1,81 @@
 package main
 
 import (
-  "testing"
-  "fmt"
-  "github.com/stretchr/testify/assert"
-  "strings"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
 )
 
 func Test_ReadInts_Should_ParseString(t *testing.T) {
-  t.Log("ReadInts should parse string '32 67'")
-  // Arrange
-  testData := "32 67"
+	t.Log("ReadInts should parse string '32 67'")
+	// Arrange
+	testData := "32 67"
 
-  // Act
-  result, err := ReadInts(strings.NewReader(testData))
+	// Act
+	result, err := ReadInts(strings.NewReader(testData))
 
-  // Assert
-  assert.Equal(t, 32, result[0])
-  assert.Equal(t, 67, result[1])
-  assert.Nil(t, err)
+	// Assert
+	assert.Equal(t, 32, result[0])
+	assert.Equal(t, 67, result[1])
+	assert.Nil(t, err)
 }
 
 func Test_FindPermut_Should_Find4PermutationOfSet3(t *testing.T) {
-  t.Log("Finds 4th permutation of 3 size set")
-  // Arrange
-  expected := [3]int{3,1,2}
-  k := 4
-  n := 3
+	t.Log("Finds 4th permutation of 3 size set")
+	// Arrange
+	expected := []int{3, 1, 2}
+	k := 4
+	n := 3
 
-  // Act
-  result := FindPermut(n, k)
+	// Act
+	result := FindPermut(n, k)
 
-  // Assert
-  assert.Equal(t, expected, result, "k'th permutation should be equal to 3 1 2")
+	// Assert
+	assert.Equal(t, expected, result, "k'th permutation should be equal to 3 1 2")
 }
 
 func Test_FindPermut_Should_Find19PermutationOfSet4(t *testing.T) {
-  t.Log("Finds 4th permutation of 3 size set")
-  // Arrange
-  expected := [4]int{4,1,3,2}
-  k := 19
-  n := 4
+	t.Log("Finds 4th permutation of 3 size set")
+	// Arrange
+	expected := []int{4, 1, 3, 2}
+	k := 19
+	n := 4
 
-  // Act
-  result := FindPermut(n, k)
+	// Act
+	result := FindPermut(n, k)
 
-  // Assert
-  assert.Equal(t, expected, result, "k'th permutation should be equal to 4 1 3 2")
+	// Assert
+	assert.Equal(t, expected, result, "k'th permutation should be equal to 4 1 3 2")
 }
 
 func Test_Factorial_Should_Be120When5(t *testing.T) {
-  // Arrange
-  expected := 120
-  n := 5
+	// Arrange
+	expected := 120
+	n := 5
 
-  // Act
-  result := Factorial(n)
+	// Act
+	result := Factorial(n)
 
-  // Assert
-  assert.Equal(t, expected, result, "factorial of 6 should be 120")
+	// Assert
+	assert.Equal(t, expected, result, "factorial of 6 should be 120")
+}
+
+func Test_Reverse_Should_ReverseArrayStartingFromK(t *testing.T) {
+	// Arrange
+	array := []int{1, 3, 5, 2, 4}
+	expected := []int{1, 3, 4, 2, 5}
+	from := 2
+
+	// Act
+	result := Reverse(array, from)
+
+	// Assert
+	assert.Equal(t, expected, result, "reverse shuld return array with reversed elements starting from k'th")
 }
 
 func BenchmarkHello(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        fmt.Sprintf("hello")
-    }
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("hello")
+	}
 }
